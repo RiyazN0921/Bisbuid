3c5c0b9f01691745c823782639f3d346820ccc36
RiyazN0921 <123182822+RiyazN0921@users.noreply.github.com>
2023-08-12 14:37:52 +0530

    Add files via upload

---
+++ HTML/ex.js
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<style>
+  /* Basic styling for the card */
+  .card {
+    border: 1px solid #ccc;
+    border-radius: 8px;
+    padding: 16px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    width: 300px;
+  }
+
+  /* Styling for the card header */
+  .card-header {
+    font-size: 18px;
+    font-weight: bold;
+    margin-bottom: 8px;
+  }
+
+  /* Styling for the card paragraph */
+  .card-paragraph {
+    font-size: 14px;
+    line-height: 1.5;
+  }
+</style>
+<title>Card Example</title>
+</head>
+<body>
+  <div class="card">
+    <div class="card-header">Card Header</div>
+    <div class="card-paragraph">
+      This is an example card with a header and a paragraph. You can customize the content and styles as needed.
+    </div>
+  </div>
+</body>
+</html>
